25d4cf2cf82d808fe51fe1bd65643d3f591d3be0
dohung <dohuyhung12@gmail.com>
2022-07-05 17:14:02 +0700

    add place holder

--- src/components/AboutBlock.tsx
+++ src/components/AboutBlock.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import main from '../assets/main.svg';
 
 const AboutBlock = () => {
   return (
-    <div id='about'>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero molestias
-      deleniti cum culpa recusandae magnam animi similique itaque repellat quod
-      nesciunt, repellendus excepturi minima, ullam harum debitis eligendi
-      sapiente a reprehenderit laborum. Totam consequatur nihil itaque,
-      consequuntur fugiat ipsam enim facilis. Molestiae, facere! Quasi soluta
-      tenetur nesciunt exercitationem dolorem optio?
+    <div id='about' className='grid'>
+      <img src={main} alt='main' />
+      <div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+          molestias deleniti cum culpa recusandae magnam animi similique itaque
+          repellat quod nesciunt, repellendus excepturi minima, ullam harum
+          debitis eligendi sapiente a reprehenderit laborum. Totam consequatur
+          nihil itaque, consequuntur fugiat ipsam enim facilis. Molestiae,
+          facere! Quasi soluta tenetur nesciunt exercitationem dolorem optio?
+        </p>
+      </div>
     </div>
   );
 };
