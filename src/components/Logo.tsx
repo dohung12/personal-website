304831681fdf8f2c801d0e6cb49cd5324a689828
dohung <dohuyhung12@gmail.com>
2022-07-05 17:14:17 +0700

    add navbar detail

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+const Logo = () => {
+  return (
+    <img
+      src={logo}
+      alt='logo'
+      style={{
+        height: '4rem',
+      }}
+    />
+  );
+};
+
+export default Logo;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,43 @@
 import ThemeButton from './ThemeButton';
+import Logo from './Logo';
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+  padding-right: 1rem;
+  .logo {
+    height: 4rem;
+    padding: 0;
+  }
+`;
+
 const Navbar = () => {
   return (
-    <nav>
-      <ThemeButton />
-    </nav>
+    <Wrapper>
+      <ul>
+        <li className='logo'>
+          <Logo />
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#skills'>Skills</a>
+        </li>
+        <li>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+        <li>
+          <ThemeButton />
+        </li>
+      </ul>
+    </Wrapper>
   );
 };
 
