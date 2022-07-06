a3b686f7af0229ded02cc4c381948358e39310e0
dohung <dohuyhung12@gmail.com>
2022-07-06 20:57:33 +0700

    complete about block

--- src/components/AboutBlock.tsx
+++ src/components/AboutBlock.tsx
@@ -1,20 +1,36 @@
+import styled from 'styled-components';
 import main from '../assets/main.svg';
+
+const Wrapper = styled.section`
+  height: calc(100vh - var(--nav-height));
+
+  .content {
+    text-align: center;
+
+    h6 {
+      text-align: start;
+    }
+  }
+`;
 
 const AboutBlock = () => {
   return (
-    <div id='about' className='grid'>
+    <Wrapper id='about' className='grid'>
       <img src={main} alt='main' />
-      <div>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
-          molestias deleniti cum culpa recusandae magnam animi similique itaque
-          repellat quod nesciunt, repellendus excepturi minima, ullam harum
-          debitis eligendi sapiente a reprehenderit laborum. Totam consequatur
-          nihil itaque, consequuntur fugiat ipsam enim facilis. Molestiae,
-          facere! Quasi soluta tenetur nesciunt exercitationem dolorem optio?
-        </p>
+      <div className='content'>
+        <hgroup>
+          <h2>Hi, I'm Do Hung</h2>
+          <h6>
+            I am a web developer. I love Javascript (and its endless list of
+            frameworks). I enjoy coding and the challenge of learning something
+            new everyday.
+          </h6>
+        </hgroup>
+        <a href='#contact' role={'button'}>
+          Get In Touch
+        </a>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
