9a1ab2d90e9ce08743dd9ddf709d14ac38ee5458
dohung <dohuyhung12@gmail.com>
2022-07-07 12:16:28 +0700

    change icon

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,5 +1,12 @@
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { BsCircleHalf } from 'react-icons/bs';
+import styled from 'styled-components';
 import { useThemeContext } from '../context/themeContext';
+
+const Wrapper = styled.label`
+  svg {
+    color: var(--primary);
+  }
+`;
 
 const ThemeButton = () => {
   const { theme, setTheme } = useThemeContext();
@@ -15,15 +22,15 @@ const ThemeButton = () => {
   };
 
   return (
-    <label htmlFor='switch'>
+    <Wrapper htmlFor='switch'>
       <input
         type='checkbox'
         name='switch'
         role='switch'
         onClick={handleClick}
       />
-      {theme === 'light' ? <FaMoon /> : <FaSun />}
-    </label>
+      <BsCircleHalf />
+    </Wrapper>
   );
 };
 
